366db9f9992355efefa6a487fd5ede440907dd32
Timothy <AGI144348@outlook.com>
2026-01-20 16:22:06 -0600

    Add interaction rules and assertion function

--- Phase_2A_I/interaction_rules.py
+++ Phase_2A_I/interaction_rules.py
@@ -1,1 +1,17 @@
+ALLOWED_INTERACTIONS = {
+    "VectorCarrier": {"ScalarCarrier"},
+    "MatrixCarrier": {"VectorCarrier"},
+    "Point": {"VectorCarrier"},
+}
 
+
+def assert_interaction_allowed(source, target):
+    source_type = type(source).__name__
+    target_type = type(target).__name__
+
+    allowed = ALLOWED_INTERACTIONS.get(source_type, set())
+
+    if target_type not in allowed:
+        raise PermissionError(
+            f"Interaction not allowed: {source_type} -> {target_type}"
+        )
