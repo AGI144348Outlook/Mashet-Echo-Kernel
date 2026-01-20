7b55e27a7e6175f01a56909735a8d9a4a6eb917a
Timothy <AGI144348@outlook.com>
2026-01-20 16:21:41 -0600

    Add data classes for carriers and related structures

--- Phase_2A_I/carriers.py
+++ Phase_2A_I/carriers.py
@@ -1,1 +1,26 @@
+from dataclasses import dataclass
 
+
+@dataclass(frozen=True)
+class ScalarCarrier:
+    state: str = "undefined"
+
+
+@dataclass(frozen=True)
+class VectorCarrier:
+    slots: tuple
+
+
+@dataclass(frozen=True)
+class MatrixCarrier:
+    rows: tuple
+
+
+@dataclass(frozen=True)
+class Point:
+    identifier: str
+
+
+@dataclass(frozen=True)
+class DimensionSlot:
+    assigned: bool = False
